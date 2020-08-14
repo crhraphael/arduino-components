7d4c60932a3b20ad0c2ac7cbe7b010b56da1c8df
Crhistian Raphael <crhistian.raphael@outlook.com>
2020-08-14 16:55:18 -0300

    feat(wifi): added wifi base structures

---
+++ include/Implementations/Wireless/WiFi/IWirelessWiFiImplementation.h
@@ -0,0 +1,13 @@
+#ifndef IWIRELESSWIFIIMPLEMENTATION
+#define IWIRELESSWIFIIMPLEMENTATION
+
+class IWirelessWiFiImplementation {
+  private:
+  public:
+  virtual ~IWirelessWiFiImplementation() = 0;
+  virtual void listen() = 0;
+	virtual void send (char *message) = 0;
+};
+
+IWirelessWiFiImplementation::~IWirelessWiFiImplementation() {};
+#endif
