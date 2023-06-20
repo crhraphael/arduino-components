b99a3f2f16309a50513cdb6ad79067e2bc15b31a
Crhistian Raphael <crhistian.raphael@outlook.com>
2023-06-20 02:28:45 -0300

    refactor(main): removing pointer call

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,16 @@
 // #include <examples/NonClassExample.h>
 // #include <examples/ESP12EVehicleExample.h>
 
-ESP12EVehicleExample* impl;
+ESP12EVehicleExample impl;
 //ESP32LolinLEDExample* implLED = new ESP32LolinLEDExample();
 
 void setup() 
 { 
-	impl = new ESP12EVehicleExample();
-  impl->setup();
-  //implLED->setup();
+	impl = ESP12EVehicleExample();
+  impl.setup();
 }  
  
 void loop()
 { 
-  impl->loop();
-  //implLED->loop();
+  impl.loop();
 } 
